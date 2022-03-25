98da29d342868b34d68d61f57fa950b162cf3c55
Jonathan Jorge <jcanola canolaj>
2022-03-25 12:58:50 -0500

    install bootstrap v5.1, fontawesome v6, add migrate post and model post

--- database/migrations/2022_03_25_173241_create_post_table.php
+++ database/migrations/2022_03_25_173241_create_post_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreatePostTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,14 +13,14 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('post', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->rememberToken();
-            $table->boolean('condition_user')->default(1);
+            $table->integer('user_id')->unsigned();
+            $table->strig('title_post');
+            $table->text('description_post');
             $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users');
         });
     }
 
@@ -31,6 +31,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('post');
     }
 }
